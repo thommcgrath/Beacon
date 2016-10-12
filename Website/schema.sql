876fb5762e41b5456fd184d691e6e3d0b3d05807
Thom McGrath <thom@thezaz.com>
2016-10-12 03:02:54 -0400

    Getting ready to store critical data online

--- Website/schema.sql
+++ Website/schema.sql
@@ -1,5 +1,6 @@
 CREATE EXTENSION "uuid-ossp";
 CREATE EXTENSION "pgcrypto";
+CREATE EXTENSION "citext";
 
 CREATE TABLE "updates" (
 	"update_id" UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -7,9 +8,9 @@ CREATE TABLE "updates" (
 	"build_display" TEXT NOT NULL,
 	"notes" TEXT NOT NULL,
 	"mac_url" TEXT NOT NULL,
-	"mac_signature" TEXT NOT NULL,
+	"mac_signature" CITEXT NOT NULL,
 	"win_url" TEXT NOT NULL,
-	"win_signature" TEXT NOT NULL
+	"win_signature" CITEXT NOT NULL
 );
 GRANT SELECT ON TABLE "updates" TO "thezaz_website";
 
@@ -28,6 +29,32 @@ CREATE TABLE "documents" (
 	"last_update" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 	"download_count" INTEGER NOT NULL DEFAULT 0,
 	"contents" TEXT NOT NULL,
-	"contents_hash" TEXT NOT NULL
+	"contents_hash" CITEXT NOT NULL
 );
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE "documents" TO "thezaz_website";
+
+CREATE TYPE "loot_source_kind" AS ENUM (
+	'Standard',
+	'Bonus',
+	'Cave',
+	'Sea'
+);
+
+CREATE TABLE "loot_sources" (
+	"classstring" CITEXT NOT NULL,
+	"label" CITEXT NOT NULL,
+	"kind" "loot_source_kind" NOT NULL,
+	"engram_mask" INTEGER NOT NULL,
+	"multiplier_min" NUMERIC(6,4) NOT NULL,
+	"multiplier_max" NUMERIC(6,4) NOT NULL
+);
+GRANT SELECT ON TABLE "loot_sources" TO "thezaz_website";
+
+CREATE TABLE "engrams" (
+	"classstring" CITEXT NOT NULL,
+	"label" CITEXT NOT NULL,
+	"availability" INTEGER NOT NULL DEFAULT 0,
+	"can_blueprint" BOOLEAN NOT NULL DEFAULT TRUE,
+	CHECK ("classstring" LIKE '%_C')
+);
+GRANT SELECT ON TABLE "engrams" TO "thezaz_website";
