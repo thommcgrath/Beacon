9b3d29812dcba425491639c2c2211ff030f0d38f
Thom McGrath <thommcgrath@users.noreply.github.com>
2017-06-10 06:03:14 +0000

    Making sure the privacy title is set.

--- Website/www/privacy.php
+++ Website/www/privacy.php
@@ -1,10 +1,8 @@
 <?php
 require($_SERVER['SITE_ROOT'] . '/framework/loader.php');
-//ZirconTemplate::SetTitle('Beacon Privacy Policy');
-?><div class="articleheader">
-	<h1>Beacon Privacy Policy</h1>
-</div>
-<div class="articlebody">
+BeaconTemplate::SetTitle('Beacon Privacy Policy');
+?><h1>Beacon Privacy Policy</h1>
+<div class="indent">
 	<p><strong>Here it is in simple non-lawyer terms</strong>: I have zero interest in your personal data. My users are not a commodity. My website doesn't even run Google Analytics.</p>
 	<p>The Beacon Announce mailing list is run by MailChimp. They handle the subscribers, double-opt-in mechanics, etc. I do not log any data about the subscribers on my own servers.</p>
 	<p>When publishing a Beacon document, your Beacon ID and public key is stored on my server. This information can be found in %AppData%\The ZAZ\Beacon\Default.beaconidentity on Windows or ~/Library/Application Support/The ZAZ/Beacon/Default.beaconidentity on Mac. The private key in that identity file NEVER leaves your computer. The identity controls ownership of the Beacon documents published online.</p>
