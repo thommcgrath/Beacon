a7f6f13b9468e77f3e5bc359e40c0fe2941345c7
Thom McGrath <thommcgrath@users.noreply.github.com>
2017-06-10 04:49:38 +0000

    Rebuilt the favicon, adding header logo, has a max-size but doesn't scale below 615px gracefully yet.

--- Website/framework/templates/default.php
+++ Website/framework/templates/default.php
@@ -11,14 +11,17 @@ if (!BeaconTemplate::IsHTML()) {
 		<meta charset="UTF-8">
 		<title><?php echo htmlentities(BeaconTemplate::Title()); ?></title>
 		<link href="/assets/css/main.css" rel="stylesheet" media="all" type="text/css">
-		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-		<link rel="manifest" href="/manifest.json">
-		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#713a9a">
+		<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
+		<link rel="manifest" href="/assets/favicon/manifest.json">
+		<link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#713a9a">
+		<link rel="shortcut icon" href="/assets/favicon/favicon.ico">
 		<meta name="apple-mobile-web-app-title" content="Beacon">
 		<meta name="application-name" content="Beacon">
+		<meta name="msapplication-config" content="/assets/favicon/browserconfig.xml">
 		<meta name="theme-color" content="#713a9a">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<?php
 		$header_lines = BeaconTemplate::ExtraHeaderLines();
 		for ($i = 0; $i < count($header_lines); $i++) {
@@ -35,16 +38,19 @@ if (!BeaconTemplate::IsHTML()) {
 	</head>
 	<body>
 		<header>
-			<div id="top-menu">
-				<a href="/spawn/">Spawn Codes</a>
-				<a href="/help/">Help</a>
-				<a href="/download/">Download</a>
+			<div class="inner">
+				<a href="/" id="header_link"><img width="251" height="40" src="/assets/images/header.svg"></a>
+				<div id="top-menu">
+					<a href="/spawn/">Spawn Codes</a>
+					<a href="/help/">Help</a>
+					<a href="/download/">Download</a>
+				</div>
 			</div>
 		</header>
-		<main>
+		<main class="inner">
 			<?php echo $buffer; ?>
 		</main>
-		<footer>
+		<footer class="inner">
 			<p><a id="footer_github_logo" href="https://github.com/thommcgrath/Beacon"><img width="96" height="26" src="/assets/images/github.svg"></a></p>
 			<p>Beacon is an open source project by Thom McGrath. Copyright 2016-<?php echo date('Y'); ?>.</p>
 			<p>Get in touch using <a href="mailto:forgotmyparachute@beaconapp.cc">forgotmyparachute@beaconapp.cc</a>.</p>
