b6fdebebc2a140c0dd084a6a5b4fe875092446ee
Thom McGrath <thommcgrath@users.noreply.github.com>
2017-06-10 02:31:18 +0000

    Making the GitHub logo smaller.

--- Website/framework/templates/default.php
+++ Website/framework/templates/default.php
@@ -37,7 +37,7 @@ if (!BeaconTemplate::IsHTML()) {
 			<?php echo $buffer; ?>
 		</main>
 		<footer>
-			<p><a id="footer_github_logo" href="https://github.com/thommcgrath/Beacon"><img width="144" height="39" src="/assets/images/github.svg"></a></p>
+			<p><a id="footer_github_logo" href="https://github.com/thommcgrath/Beacon"><img width="96" height="26" src="/assets/images/github.svg"></a></p>
 			<p>Beacon is an open source project by Thom McGrath. Copyright 2016-<?php echo date('Y'); ?>.</p>
 			<p>Get in touch using <a href="mailto:forgotmyparachute@beaconapp.cc">forgotmyparachute@beaconapp.cc</a>.</p>
 		</footer>
