dc9a09d9c14d0af7ac4f09f80db77785145cd8d2
Thom McGrath <thommcgrath@users.noreply.github.com>
2018-01-15 22:12:48 +0000

    Making the ftp API a little easier to use.

--- Website/api/v1/ftp.php
+++ Website/api/v1/ftp.php
@@ -13,17 +13,18 @@ $ftp_path = isset($_REQUEST['path']) ? $_REQUEST['path'] : BeaconAPI::ReplyError
 if ($ftp_port == 0) {
 	$ftp_port = 21;
 }
+$ref = isset($_REQUEST['ref']) ? $_REQUEST['ref'] : BeaconCommon::GenerateUUID();
 
 if (strtolower(substr($ftp_path, -4)) != '.ini') {
-	BeaconAPI::ReplyError('Requested file does not appear to be an ini file.', $ftp_path, 500);
+	BeaconAPI::ReplyError('Requested file does not appear to be an ini file.', array('ref' => $ref, 'path' => $ftp_path), 500);
 }
 
 $connection = ftp_connect($ftp_host, $ftp_port);
 if ($connection === false) {
-	BeaconAPI::ReplyError('Unable to connect to host.', $ftp_host . ':' . $ftp_port, 500);
+	BeaconAPI::ReplyError('Unable to connect to host.', array('ref' => $ref, 'host' => $ftp_host . ':' . $ftp_port), 500);
 }
 if (!@ftp_login($connection, $ftp_user, $ftp_pass)) {
-	BeaconAPI::ReplyError('Unable to authenticate with host.', null, 500);
+	BeaconAPI::ReplyError('Unable to authenticate with host.', array('ref' => $ref), 500);
 }
 ftp_pasv($connection, true);
 
@@ -35,18 +36,19 @@ case 'GET':
 	
 	if ($success) {
 		header('Content-Type: text/plain');
-		readfile($temp_path);
+		$content = file_get_contents($temp_path);
 		unlink($temp_path);
+		BeaconAPI::ReplySuccess(array('ref' => $ref, 'content' => $content));
 	} else {
 		if (file_exists($temp_path)) {
 			unlink($temp_path);
 		}
-		BeaconAPI::ReplyError('Unable to download file.', $ftp_path, 500);
+		BeaconAPI::ReplyError('Unable to download file.', array('ref' => $ref, 'path' => $ftp_path), 500);
 	}
 	break;
 case 'POST':
 	if (BeaconAPI::ContentType() !== 'text/plain') {
-		BeaconAPI::ReplyError('Send plain text data.');
+		BeaconAPI::ReplyError('Send plain text data.', array('ref' => $ref), 400);
 	}
 	
 	$temp_path = sys_get_temp_dir() . '/' . BeaconCommon::GenerateUUID();
@@ -56,18 +58,18 @@ case 'POST':
 	ftp_close($connection);
 	
 	if ($success) {
-		BeaconAPI::ReplySuccess();
+		BeaconAPI::ReplySuccess(array('ref' => $ref));
 		unlink($temp_path);
 	} else {
 		if (file_exists($temp_path)) {
 			unlink($temp_path);
 		}
-		BeaconAPI::ReplyError('Unable to upload file.', $ftp_path, 500);
+		BeaconAPI::ReplyError('Unable to upload file.', array('ref' => $ref, 'path' => $ftp_path), 500);
 	}
 	
 	break;
 default:
-	BeaconAPI::ReplyError('Method not allowed.', null, 405);
+	BeaconAPI::ReplyError('Method not allowed.', array('ref' => $ref), 405);
 	break;
 }
 
