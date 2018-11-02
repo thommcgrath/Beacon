f9559f6d07bc7c4bb2e68e60c77b97646267bc71
Thom McGrath <thommcgrath@users.noreply.github.com>
2018-11-02 01:38:01 -0400

    Mac build script has automated notarization.

--- Installers/Mac/Build.sh
+++ Installers/Mac/Build.sh
@@ -31,3 +31,26 @@ hdiutil convert "${OUTPUT}/${APPNAME}.sparseimage" -format UDBZ -o "${OUTPUT}/${
 hdiutil internet-enable -yes "${OUTPUT}/${APPNAME}.dmg";
 codesign -s 'Developer ID Application: Thom McGrath' "${OUTPUT}/${APPNAME}.dmg";
 rm -rf "${OUTPUT}/${APPNAME}.sparseimage";
+
+echo "Uploading disk image for notarization. This can take a while.";
+APPLEID=$(security find-generic-password -s "App Notarization" | sed -n 's/^[[:space:]]*"acct"<blob>="\(.*\)"/\1/p');
+TEAMID=$(security find-generic-password -s "App Notarization" | sed -n 's/^[[:space:]]*"icmt"<blob>="\(.*\)"/\1/p');
+xcrun altool --notarize-app --file "${OUTPUT}/${APPNAME}.dmg" --primary-bundle-id com.thezaz.beacon --username "${APPLEID}" --password @keychain:"App Notarization" --asc-provider "${TEAMID}" > ${TMPDIR}notarize_output 2>&1 || { rm -f ${TMPDIR}notarize_output; echo "Failed to upload disk image for notarization"; exit $?; };
+REQUESTUUID=$(sed -n 's/RequestUUID = \(.*\)/\1/p' ${TMPDIR}notarize_output);
+echo "Disk image has been uploaded. Request UUID is ${REQUESTUUID}. Checking status every 10 seconds...";
+STATUS="in progress";
+while [ "${STATUS}" = "in progress" ]; do
+	sleep 10s;
+	xcrun altool --notarization-info "${REQUESTUUID}" --username "${APPLEID}" --password @keychain:"App Notarization" --asc-provider "${TEAMID}" > ${TMPDIR}notarize_output 2>&1 || { rm -f ${TMPDIR}notarize_output; echo "Failed to check on notarization status."; exit $?; };
+	STATUS=$(sed -ne 's/^[[:space:]]*Status: \(.*\)$/\1/p' ${TMPDIR}notarize_output);
+done;
+rm -f ${TMPDIR}notarize_output;
+if [ "${STATUS}" = "success" ]; then
+	xcrun stapler staple -v "${OUTPUT}/${APPNAME}.dmg";
+else
+	echo "Disk image was not notarized, status is ${STATUS}.";
+	exit 1;
+fi;
+
+echo "Disk image is ready for shipping.";
+exit 0;
