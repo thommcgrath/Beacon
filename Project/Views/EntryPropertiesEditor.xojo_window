thommcgrath/Beacon

commit 1c3f35e12fc49749cb2c48bc2b04edf896277567

Giving item set entries the same weight treatment.

Project/Views/EntryPropertiesEditor.xojo_window
@@ -676,7 +676,7 @@ Begin BeaconContainer EntryPropertiesEditor
       LockLeft        =   True
       LockRight       =   True
       LockTop         =   True
-      Maximum         =   100
+      Maximum         =   1000
       Minimum         =   0
       PageStep        =   25
       Scope           =   2
@@ -686,7 +686,7 @@ Begin BeaconContainer EntryPropertiesEditor
       TickStyle       =   "0"
       Top             =   176
       Transparent     =   False
-      Value           =   25
+      Value           =   250
       Visible         =   True
       Width           =   139
    End
@@ -721,7 +721,7 @@ Begin BeaconContainer EntryPropertiesEditor
       TabIndex        =   19
       TabPanelIndex   =   0
       TabStop         =   True
-      Text            =   "25"
+      Text            =   "250"
       TextColor       =   &c00000000
       TextFont        =   "System"
       TextSize        =   0.0
@@ -836,7 +836,7 @@ End
 		      Entry.ChanceToBeBlueprint = ChanceSlider.Value / 100
 		    End If
 		    If EditWeightCheck.Value Then
-		      Entry.Weight = WeightSlider.Value / 100
+		      Entry.RawWeight = WeightSlider.Value
 		    End If
 		    If EditMaxQualityCheck.Value Then
 		      Entry.MaxQuality = MaxQuality
@@ -909,7 +909,7 @@ End
 		  For Each Entry As Beacon.SetEntry In Entries
 		    MinQuantities.Append(Entry.MinQuantity)
 		    MaxQuantities.Append(Entry.MaxQuantity)
-		    TotalWeight = TotalWeight + Entry.Weight
+		    TotalWeight = TotalWeight + Entry.RawWeight
 		    TotalChance = TotalChance + Entry.ChanceToBeBlueprint
 		    MinQualities.Append(Entry.MinQuality.BaseValue)
 		    MaxQualities.Append(Entry.MaxQuality.BaseValue)
@@ -925,7 +925,7 @@ End
 		  MinQuantityField.Text = Str(MinQuantities(0))
 		  MaxQuantityField.Text = Str(MaxQuantities(UBound(MaxQuantities)))
 		  If CanBeBlueprint Then
-		    ChanceSlider.Value = 100 * (TotalChance / (UBound(Entries) + 1))
+		    ChanceSlider.Value = TotalChance / (Entries.Ubound + 1)
 		    ChanceSlider.Enabled = True
 		    ChanceLabel.Enabled = True
 		    ChanceField.Enabled = True
@@ -938,8 +938,8 @@ End
 		    EditChanceCheck.Enabled = False
 		  End If
 		  MinQualityMenu.SelectByTag(MinQualities(0))
-		  MaxQualityMenu.SelectByTag(MaxQualities(UBound(MaxQualities)))
-		  WeightSlider.Value = 100 * (TotalWeight / (UBound(Entries) + 1))
+		  MaxQualityMenu.SelectByTag(MaxQualities(MaxQualities.Ubound))
+		  WeightSlider.Value = TotalWeight / (Entries.Ubound + 1)
 		  Self.mIgnoreChanges = False
 		  
 		  If UBound(Entries) > 0 Then

Project/Modules/Beacon/ItemSet.xojo_code
@@ -15,10 +15,10 @@ Implements Beacon.Countable,Beacon.DocumentItem
 		  WeightSum = 0
 		  
 		  For Each Entry As Beacon.SetEntry In Pool
-		    If Entry.Weight = 0 Then
+		    If Entry.RawWeight = 0 Then
 		      Continue
 		    End If
-		    WeightSum = WeightSum + Entry.Weight
+		    WeightSum = WeightSum + Entry.RawWeight
 		    Weights.Append(WeightSum * WeightScale)
 		    WeightLookup.Value(WeightSum * WeightScale) = Entry
 		  Next
@@ -89,7 +89,7 @@ Implements Beacon.Countable,Beacon.DocumentItem
 		  Keys.Value("MaxNumItems") = Self.MaxNumItems
 		  Keys.Value("MinNumItems") = Self.MinNumItems
 		  Keys.Value("NumItemsPower") = Self.NumItemsPower
-		  Keys.Value("RawWeight") = Self.RawWeight
+		  Keys.Value("Weight") = Self.RawWeight
 		  Keys.Value("SetWeight") = Self.RawWeight / 1000
 		  If Self.SourcePresetID <> "" Then
 		    Keys.Value("SourcePresetID") = Self.SourcePresetID
@@ -221,14 +221,10 @@ Implements Beacon.Countable,Beacon.DocumentItem
 		  If Dict.HasKey("NumItemsPower") Then
 		    Set.NumItemsPower = Dict.Value("NumItemsPower")
 		  End If
-		  If Dict.HasKey("RawWeight") Then
-		    Set.RawWeight = Dict.Value("RawWeight")
-		  ElseIf Dict.HasKey("SetWeight") Then
-		    // If you don't do it this way, Xojo reads it as an integer... wtf.
-		    Dim Percentage As Double = Dict.Value("SetWeight")
-		    Set.RawWeight = Percentage * 1000
-		  ElseIf Dict.HasKey("Weight") Then
+		  If Dict.HasKey("Weight") Then
 		    Set.RawWeight = Dict.Value("Weight")
+		  ElseIf Dict.HasKey("SetWeight") Then
+		    Set.RawWeight = Dict.Value("SetWeight") * 1000.0
 		  End If
 		  If Dict.HasKey("bItemsRandomWithoutReplacement") Then
 		    Set.ItemsRandomWithoutReplacement = Dict.Value("bItemsRandomWithoutReplacement")
@@ -436,7 +432,7 @@ Implements Beacon.Countable,Beacon.DocumentItem
 
 	#tag Method, Flags = &h0
 		Function RelativeWeight(Index As Integer) As Double
-		  Return Self.mEntries(Index).Weight / Self.TotalWeight()
+		  Return Self.mEntries(Index).RawWeight / Self.TotalWeight()
 		End Function
 	#tag EndMethod
 
@@ -548,7 +544,7 @@ Implements Beacon.Countable,Beacon.DocumentItem
 		Function TotalWeight() As Double
 		  Dim Value As Double
 		  For Each Entry As Beacon.SetEntry In Self.mEntries
-		    Value = Value + Entry.Weight
+		    Value = Value + Entry.RawWeight
 		  Next
 		  Return Value
 		End Function

Project/Modules/Beacon/SetEntry.xojo_code
@@ -57,7 +57,7 @@ Implements Beacon.Countable,Beacon.DocumentItem
 		  Self.mMinQuality = Beacon.Qualities.Tier1
 		  Self.mMaxQuality = Beacon.Qualities.Tier3
 		  Self.mChanceToBeBlueprint = 1.0
-		  Self.mWeight = 1
+		  Self.mWeight = 250
 		  Self.mUniqueID = ""
 		End Sub
 	#tag EndMethod
@@ -185,7 +185,8 @@ Implements Beacon.Countable,Beacon.DocumentItem
 		  Keys.Value("MaxQuality") = Self.MaxQuality.Key
 		  Keys.Value("MinQuantity") = Self.MinQuantity
 		  Keys.Value("MaxQuantity") = Self.MaxQuantity
-		  Keys.Value("EntryWeight") = Self.Weight
+		  Keys.Value("Weight") = Self.RawWeight
+		  Keys.Value("EntryWeight") = Self.RawWeight / 1000
 		  Return Keys
 		End Function
 	#tag EndMethod
@@ -216,7 +217,7 @@ Implements Beacon.Countable,Beacon.DocumentItem
 		    Parts(3) = Self.MaxQuantity.ToText(Locale, Format)
 		    Parts(4) = Self.MinQuality.Key.Lowercase
 		    Parts(5) = Self.MinQuantity.ToText(Locale, Format)
-		    Parts(6) = Self.Weight.ToText(Locale, Format)
+		    Parts(6) = Self.RawWeight.ToText(Locale, Format)
 		    
 		    Self.mHash = Beacon.MD5(Parts.Join(",")).Lowercase
 		    Self.mLastHashTime = Microseconds
@@ -242,10 +243,10 @@ Implements Beacon.Countable,Beacon.DocumentItem
 	#tag Method, Flags = &h0
 		Shared Function ImportFromBeacon(Dict As Xojo.Core.Dictionary) As Beacon.SetEntry
 		  Dim Entry As New Beacon.SetEntry
-		  If Dict.HasKey("EntryWeight") Then
-		    Entry.Weight = Dict.Value("EntryWeight")
-		  ElseIf Dict.HasKey("Weight") Then
-		    Entry.Weight = Dict.Value("Weight")
+		  If Dict.HasKey("Weight") Then
+		    Entry.RawWeight = Dict.Value("Weight")
+		  ElseIf Dict.HasKey("EntryWeight") Then
+		    Entry.RawWeight = Dict.Value("EntryWeight") * 1000.0
 		  End If
 		  If Dict.HasKey("MinQuality") Then
 		    Entry.MinQuality = Beacon.Qualities.ForKey(Dict.Value("MinQuality"))
@@ -277,7 +278,7 @@ Implements Beacon.Countable,Beacon.DocumentItem
 		Shared Function ImportFromConfig(Dict As Xojo.Core.Dictionary, Multipliers As Beacon.Range, DifficultyValue As Double) As Beacon.SetEntry
 		  Dim Entry As New Beacon.SetEntry
 		  If Dict.HasKey("EntryWeight") Then
-		    Entry.Weight = Dict.Value("EntryWeight")
+		    Entry.RawWeight = Dict.Value("EntryWeight")
 		  End If
 		  
 		  If Dict.HasKey("MinQuality") Then
@@ -413,12 +414,8 @@ Implements Beacon.Countable,Beacon.DocumentItem
 	#tag Method, Flags = &h0
 		Shared Function Join(Entries() As Beacon.SetEntry, Separator As Text, Multipliers As Beacon.Range, UseBlueprints As Boolean, Difficulty As BeaconConfigs.Difficulty) As Text
 		  Dim Values() As Text
-		  Dim SumEntryWeights As Double
 		  For Each Entry As Beacon.SetEntry In Entries
-		    SumEntryWeights = SumEntryWeights + Entry.Weight
-		  Next
-		  For Each Entry As Beacon.SetEntry In Entries
-		    Values.Append(Entry.TextValue(Multipliers, SumEntryWeights, UseBlueprints, Difficulty))
+		    Values.Append(Entry.TextValue(Multipliers, UseBlueprints, Difficulty))
 		  Next
 		  Return Values.Join(Separator)
 		End Function
@@ -573,7 +570,7 @@ Implements Beacon.Countable,Beacon.DocumentItem
 	#tag EndMethod
 
 	#tag Method, Flags = &h0
-		Function TextValue(Multipliers As Beacon.Range, SumEntryWeights As Double, UseBlueprints As Boolean, Difficulty As BeaconConfigs.Difficulty) As Text
+		Function TextValue(Multipliers As Beacon.Range, UseBlueprints As Boolean, Difficulty As BeaconConfigs.Difficulty) As Text
 		  Dim Paths(), Weights(), Classes() As Text
 		  Redim Paths(UBound(Self.mOptions))
 		  Redim Weights(UBound(Self.mOptions))
@@ -597,7 +594,7 @@ Implements Beacon.Countable,Beacon.DocumentItem
 		  End If
 		  
 		  Dim Chance As Double = if(Self.CanBeBlueprint, Self.mChanceToBeBlueprint, 0)
-		  Dim EntryWeight As Integer = Xojo.Math.Round((Self.mWeight / SumEntryWeights) * 1000)
+		  Dim EntryWeight As Integer = Self.mWeight
 		  
 		  Dim Values() As Text
 		  Values.Append("EntryWeight=" + EntryWeight.ToText)
@@ -807,7 +804,7 @@ Implements Beacon.Countable,Beacon.DocumentItem
 		#tag EndGetter
 		#tag Setter
 			Set
-			  Value = Min(Max(Value, 0.0001), 1.0)
+			  Value = Max(Value, 0.0001)
 			  If Self.mWeight = Value Then
 			    Return
 			  End If
@@ -816,7 +813,7 @@ Implements Beacon.Countable,Beacon.DocumentItem
 			  Self.Modified = True
 			End Set
 		#tag EndSetter
-		Weight As Double
+		RawWeight As Double
 	#tag EndComputedProperty
 
 

Project/Views/Config Editors/Loot Config Editor/ItemSetEditor.xojo_window
@@ -152,7 +152,6 @@ Begin BeaconContainer ItemSetEditor
       HasBackColor    =   False
       Height          =   23
       HelpTag         =   ""
-      Index           =   -2147483648
       InitialParent   =   ""
       Left            =   0
       LockBottom      =   False
@@ -348,7 +347,7 @@ End
 		      QuantityText = Entry.MinQuantity.ToText + " - " + Entry.MaxQuantity.ToText
 		    End If
 		    
-		    Dim FiguresText As String = Str(Round(Entry.Weight * 100), "0") + " wt"
+		    Dim FiguresText As String = Str(Round(Entry.RawWeight), "0") + " wt"
 		    If Entry.CanBeBlueprint Then
 		      FiguresText = FiguresText + ", " + Str(BlueprintChance, "0%") + " bp"
 		    End If
@@ -554,8 +553,8 @@ End
 		      Value2 = Entry2.MinQuality.BaseValue
 		    End If
 		  Case Self.ColumnFigures
-		    Value1 = Entry1.Weight
-		    Value2 = Entry2.Weight
+		    Value1 = Entry1.RawWeight
+		    Value2 = Entry2.RawWeight
 		  End Select
 		  
 		  If Value1 = Value2 Then

commit 6c80af3014e40bc9f12287119d6ca7d81dc6b06e

Fixing entry editor chance value mistakenly broken in 1c3f35e12fc49749cb2c48bc2b04edf896277567

Project/Views/EntryPropertiesEditor.xojo_window
@@ -925,7 +925,7 @@ End
 		  MinQuantityField.Text = Str(MinQuantities(0))
 		  MaxQuantityField.Text = Str(MaxQuantities(UBound(MaxQuantities)))
 		  If CanBeBlueprint Then
-		    ChanceSlider.Value = TotalChance / (Entries.Ubound + 1)
+		    ChanceSlider.Value = 100 * (TotalChance / (Entries.Ubound + 1))
 		    ChanceSlider.Enabled = True
 		    ChanceLabel.Enabled = True
 		    ChanceField.Enabled = True