c8c66aa56fa17e8164ec2c4dfd47fd57fd725603
Thom McGrath <thommcgrath@users.noreply.github.com>
2019-06-21 20:35:23 +0000

    Adding creatures to website mods page and spawn codes list.

--- Website/www/mods/info.php
+++ Website/www/mods/info.php
@@ -20,9 +20,11 @@ if (is_null($mod)) {
 BeaconTemplate::SetTitle('Mod: ' . $mod->Name());
 $engrams = BeaconEngram::Get($mod->ModID());
 $loot_sources = BeaconLootSource::Get($mod->ModID());
+$creatures = BeaconCreature::Get($mod->ModID());
 $has_engrams = count($engrams) > 0;
 $has_loot_sources = count($loot_sources) > 0;
-$has_something = $has_engrams || $has_loot_sources;
+$has_creatures = count($creatures) > 0;
+$has_something = $has_engrams || $has_loot_sources || $has_creatures;
 
 ?><h1><?php echo htmlentities($mod->Name()); ?></h1>
 <p>Beacon has built-in support for <a href="<?php echo BeaconWorkshopItem::URLForModID($mod->WorkshopID()); ?>"><?php echo htmlentities($mod->Name()); ?></a>. This means its engrams are already part of Beacon's database and you can begin using them immediately.</p>
@@ -36,6 +38,12 @@ $has_something = $has_engrams || $has_loot_sources;
 <h3><?php echo htmlentities($mod->Name()); ?> Loot Sources</h3>
 <ul>
 	<?php foreach ($loot_sources as $loot_source) { ?><li><a href="/object/<?php echo $loot_source->ClassString(); ?>"><?php echo htmlentities($loot_source->Label()); ?></a></li><?php } ?>
+</ul>
+<?php } ?>
+<?php if ($has_creatures) { ?>
+<h3><?php echo htmlentities($mod->Name()); ?> Creatures</h3>
+<ul>
+	<?php foreach ($creatures as $creature) { ?><li><a href="/object/<?php echo $creature->ClassString(); ?>"><?php echo htmlentities($creature->Label()); ?></a></li><?php } ?>
 </ul>
 <?php } ?>
 <?php if (!$has_something) { ?>
