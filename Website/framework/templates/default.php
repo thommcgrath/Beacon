3781cae114112eddfeb86c0920fad1af104ad45d
Thom McGrath <thommcgrath@users.noreply.github.com>
2019-07-01 22:32:05 +0000

    Hopefully fixing Add to Home Screen url.

--- Website/framework/templates/default.php
+++ Website/framework/templates/default.php
@@ -16,7 +16,6 @@ if (empty($description)) {
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php echo htmlentities($description); ?>">
-		<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
 		<link rel="manifest" href="/assets/favicon/manifest.json">
