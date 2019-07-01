77eff5fd423a66cb43c19441791d260817d22b4b
Thom McGrath <thommcgrath@users.noreply.github.com>
2019-07-01 15:31:54 +0000

    Downloads page and updates checker supports separated architecture installers.

--- Website/www/download/index.php
+++ Website/www/download/index.php
@@ -2,58 +2,167 @@
 require(dirname(__FILE__, 3) . '/framework/loader.php');
 
 $database = BeaconCommon::Database();
-$results = $database->Query("SELECT mac_url, win_url, build_display, build_number FROM updates WHERE stage >= 3 ORDER BY build_number DESC LIMIT 1;");
+$results = $database->Query("SELECT mac_url, win_32_url, win_64_url, win_combo_url, build_display, build_number FROM updates WHERE stage >= 3 ORDER BY build_number DESC LIMIT 1;");
 if ($results->RecordCount() != 1) {
 	echo 'Whoops, no version information was found.';
 	exit;
 }
-$mac_url = $results->Field('mac_url');
-$win_url = $results->Field('win_url');
+
+$primary_links = array();
+$alternate_links = array();
 $version = $results->Field('build_display');
-$build   = intval($results->Field('build_number'));
+$build = intval($results->Field('build_number'));
+
+if (BeaconCommon::IsMacOS()) {
+	$primary_links[] = array(
+		'label' => 'Download for Mac',
+		'url' => $results->Field('mac_url')
+	);
+	
+	if (is_null($results->Field('win_64_url')) === false) {
+		$alternate_links[] = array(
+			'label' => 'Download for Windows (64-bit)',
+			'url' => $results->Field('win_64_url')
+		);
+	}
+	if (is_null($results->Field('win_32_url')) === false) {
+		$alternate_links[] = array(
+			'label' => 'Download for Windows (32-bit)',
+			'url' => $results->Field('win_32_url')
+		);
+	}
+	if ((is_null($results->Field('win_64_url')) || is_null($results->Field('win_32_url'))) && is_null($results->Field('win_combo_url')) === false) {
+		$alternate_links[] = array(
+			'label' => 'Download for Windows',
+			'url' => $results->Field('win_combo_url')
+		);
+	}
+} elseif (BeaconCommon::IsWindows()) {
+	$is_64 = BeaconCommon::IsWindows64();
+	
+	if ($is_64 && is_null($results->Field('win_64_url')) === false) {
+		$primary_links[] = array(
+			'label' => 'Download for Windows',
+			'url' => $results->Field('win_64_url')
+		);
+		
+		if (is_null($results->Field('win_32_url')) === false) {
+			$alternate_links[] = array(
+				'label' => 'Download for Windows (32-bit)',
+				'url' => $results->Field('win_32_url')
+			);
+		}
+	} elseif ($is_64 === false && is_null($results->Field('win_32_url')) === false) {
+		$primary_links[] = array(
+			'label' => 'Download for Windows',
+			'url' => $results->Field('win_32_url')
+		);
+		
+		if (is_null($results->Field('win_64_url')) === false) {
+			$alternate_links[] = array(
+				'label' => 'Download for Windows (64-bit)',
+				'url' => $results->Field('win_64_url')
+			);
+		}
+	} elseif (is_null($results->Field('win_combo_url')) === false) {
+		$primary_links[] = array(
+			'label' => 'Download for Windows',
+			'url' => $results->Field('win_combo_url')
+		);
+	} elseif (is_null($results->Field('win_64_url')) === false) {
+		$primary_links[] = array(
+			'label' => 'Download for Windows',
+			'url' => $results->Field('win_64_url')
+		);
+	} elseif (is_null($results->Field('win_32_url')) === false) {
+		$primary_links[] = array(
+			'label' => 'Download for Windows',
+			'url' => $results->Field('win_32_url')
+		);
+	}
+	
+	$alternate_links[] = array(
+		'label' => 'Download for Mac',
+		'url' => $results->Field('mac_url')
+	);
+} else {
+	$primary_links[] = array(
+		'label' => 'Download for Mac',
+		'url' => $results->Field('mac_url')
+	);
+	
+	if (is_null($results->Field('win_64_url')) === false) {
+		$primary_links[] = array(
+			'label' => 'Download for Windows (64-bit)',
+			'url' => $results->Field('win_64_url')
+		);
+	}
+	if (is_null($results->Field('win_32_url')) === false) {
+		$primary_links[] = array(
+			'label' => 'Download for Windows (32-bit)',
+			'url' => $results->Field('win_32_url')
+		);
+	}
+	if ((is_null($results->Field('win_64_url')) || is_null($results->Field('win_32_url'))) && is_null($results->Field('win_combo_url')) === false) {
+		$primary_links[] = array(
+			'label' => 'Download for Windows',
+			'url' => $results->Field('win_combo_url')
+		);
+	}
+}
+
+$alternate_links[] = array(
+	'label' => 'System Requirements',
+	'url' => '#requirements'
+);
+
+$alternate_html = array();
+foreach ($alternate_links as $link) {
+	$alternate_html[] = '<a href="' . htmlentities($link['url']) . '" rel="nofollow">' . htmlentities($link['label']) . '</a>';
+}
 
 $results = $database->Query("SELECT MAX(stamp) AS stamp FROM ((SELECT MAX(objects.last_update) AS stamp FROM objects INNER JOIN mods ON (objects.mod_id = mods.mod_id) WHERE objects.min_version <= $1 AND mods.confirmed = TRUE) UNION (SELECT MAX(action_time) AS stamp FROM deletions WHERE min_version <= $1) UNION (SELECT MAX(last_update) AS stamp FROM help_topics) UNION (SELECT MAX(last_update) AS stamp FROM game_variables)) AS merged;", $build);
 $last_database_update = new DateTime($results->Field("stamp"), new DateTimeZone('UTC'));
-$prerelease = $database->Query("SELECT mac_url, win_url, build_display, build_number, stage FROM updates WHERE stage < 3 AND build_number > $1 ORDER BY build_number DESC LIMIT 1;", $build);
+$prerelease = $database->Query("SELECT mac_url, win_64_url, win_combo_url, win_32_url, build_display, build_number, stage FROM updates WHERE stage < 3 AND build_number > $1 ORDER BY build_number DESC LIMIT 1;", $build);
 
-define('MODE_NA', 0);
-define('MODE_MAC', 1);
-define('MODE_WIN', 2);
-
-$mac_label = 'Download for Mac';
-$win_label = 'Download for Windows';
-
-$mode = MODE_NA;
-if (BeaconCommon::IsMacOS()) {
-	$mode = MODE_MAC;
-	$primary_label = $mac_label;
-	$primary_url = $mac_url;
-	$alternate_label = $win_label;
-	$alternate_url = $win_url;
-} elseif (BeaconCommon::IsWindows()) {
-	$mode = MODE_WIN;
-	$primary_label = $win_label;
-	$primary_url = $win_url;
-	$alternate_label = $mac_label;
-	$alternate_url = $mac_url;
-}
-?>
-<h1>Current Version</h1>
+?><h1>Current Version</h1>
 <p class="text-center">Version <?php echo $version; ?></p>
-<?php if ($mode == MODE_NA) { ?>
-<p class="text-center"><a class="button" href="<?php echo $mac_url; ?>" rel="nofollow"><?php echo htmlentities($mac_label); ?></a><a class="button" href="<?php echo $win_url; ?>" rel="nofollow"><?php echo htmlentities($win_label); ?></a><br><span class="mini"><a href="#requirements">System Requirements</a></span></p>
-<?php } else { ?>
-<p class="text-center"><a class="button" href="<?php echo $primary_url; ?>" rel="nofollow"><?php echo htmlentities($primary_label); ?></a><br><span class="mini">Or <a href="<?php echo $alternate_url; ?>" rel="nofollow"><?php echo htmlentities($alternate_label); ?></a> | <a href="#requirements">System Requirements</a></span></p>
-<?php } ?>
+<p class="text-center"><?php foreach ($primary_links as $link) { ?><a class="button" href="<?php echo $link['url']; ?>" rel="nofollow"><?php echo htmlentities($link['label']); ?></a><?php } ?><br><span class="mini"><?php echo implode(' | ' , $alternate_html); ?></span></p>
 <h3>Engrams Database</h3>
 <div class="indent">
 	<p><a href="classes.php?version=<?php echo $build; ?>" rel="nofollow">Download Engrams Database</a><br>Last updated <?php echo '<time datetime="' . $last_database_update->format('c') . '">' . $last_database_update->format('F jS, Y') . ' at ' . $last_database_update->format('g:i A') . ' UTC</time>'; ?>.</p>
 </div>
-<?php if ($prerelease->RecordCount() == 1) { ?>
+<?php if ($prerelease->RecordCount() == 1) {
+	
+	$prerelease_links = array('<a href="' . htmlentities($prerelease->Field('mac_url')) . '" rel="nofollow">Mac Download</a>');
+	$is_64 = BeaconCommon::IsMacOS() || BeaconCommon::IsWindows64();
+	
+	if (is_null($prerelease->Field('win_64_url')) && is_null($prerelease->Field('win_32_url'))) {
+		$prerelease_links[] = '<a href="' . $prerelease->Field('win_combo_url') . '" rel="nofolow">Windows Download</a>';
+	} else {
+		if (is_null($prerelease->Field('win_64_url')) === false) {
+			$label = 'Windows Download';
+			if ($is_64 === false && is_null($prerelease->Field('win_32_url')) === false) {
+				$label .= ' (64-bit)';
+			}
+			$prerelease_links[] = '<a href="' . $prerelease->Field('win_64_url') . '" rel="nofolow">' . $label . '</a>';
+		}
+		if (is_null($prerelease->Field('win_32_url')) === false) {
+			$label = 'Windows Download';
+			if ($is_64 === true && is_null($prerelease->Field('win_64_url')) === false) {
+				$label .= ' (32-bit)';
+			}
+			$prerelease_links[] = '<a href="' . $prerelease->Field('win_32_url') . '" rel="nofolow">' . $label . '</a>';
+		}
+	}
+	
+	$prerelease_links[] = '<a href="/history.php?stage=' . htmlentities($prerelease->Field('stage')) . '#build' . htmlentities($prerelease->Field('build_number')) . '">Release Notes</a>';
+	$prerelease_links[] = '<a href="classes.php?version=' . $prerelease->Field('build_number') . '" rel="nofollow">Engrams Database</a>';
+?>
 <h3 id="preview">Preview Release</h3>
 <div class="indent">
 	<p>Beacon <?php echo htmlentities($prerelease->Field('build_display')); ?> is available for testing. Preview releases may not be stable and users should make backups of any data they are not willing to lose. To create a backup, launch Beacon and choose &quot;Open Data Folder&quot; from the &quot;Help&quot; menu. The folder shown contains valuable user data. Copy the folder to a safe location, along with any Beacon files desired.</p>
-	<p>Links: <a href="<?php echo htmlentities($prerelease->Field('mac_url')); ?>" rel="nofollow">Mac Download</a>, <a href="<?php echo htmlentities($prerelease->Field('win_url')); ?>" rel="nofollow">Windows Download</a>, <a href="/history.php?stage=<?php echo htmlentities($prerelease->Field('stage')); ?>#build<?php echo htmlentities($prerelease->Field('build_number')); ?>">Release Notes</a>, <a href="classes.php?version=<?php echo $prerelease->Field('build_number'); ?>" rel="nofollow">Engrams Database</a></p>
+	<p>Links: <?php echo implode(', ', $prerelease_links); ?></p>
 </div>
 <?php } ?>
 <h3 id="requirements">System Requirements</h3>
