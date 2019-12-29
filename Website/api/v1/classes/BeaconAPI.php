thommcgrath/Beacon

commit 856e1f66427bfa04b20b7af9f33af02c90008e45

Fixed session creation from public key authentication.

Website/api/v1/classes/BeaconAPI.php
@@ -128,7 +128,7 @@ abstract class BeaconAPI {
 							$content .= chr(10) . self::Body();
 						}
 						
-						if ($user->CheckSignature($content, hex2bin($password))) {
+						if ($user->CheckSignature($content, $password)) {
 							self::$user_id = $username;
 							self::$auth_style = self::AUTH_STYLE_PUBLIC_KEY;
 							$authorized = true;

commit 9c3723af9e4c3d60f4c3fd47a06df55c3a620983

Fixing bug in identity-based authentication caused in 856e1f66427bfa04b20b7af9f33af02c90008e45

Website/api/v1/classes/BeaconAPI.php
@@ -128,6 +128,10 @@ abstract class BeaconAPI {
 							$content .= chr(10) . self::Body();
 						}
 						
+						if (BeaconCommon::IsHex($password)) {
+							$password = hex2bin($password);
+						}
+						
 						if ($user->CheckSignature($content, $password)) {
 							self::$user_id = $username;
 							self::$auth_style = self::AUTH_STYLE_PUBLIC_KEY;

Website/framework/classes/BeaconCommon.php
@@ -535,6 +535,14 @@ abstract class BeaconCommon {
 		}
 		return number_format($num, $i);
 	}
+	
+	public static function IsHex($content) {
+		if (!is_string($content)) {
+			return false;
+		}
+		
+		return (preg_match('/^[a-fA-F0-9]+$/', $content) === 1);
+	}
 }
 
 ?>