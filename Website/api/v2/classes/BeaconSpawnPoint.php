c9de1f9f071f78c616e5dc37df642ddf5261205d
Thom McGrath <thommcgrath@users.noreply.github.com>
2020-07-15 07:05:01 +0000

    v2 API has significantly more detail in its spawn point defaults.

--- Website/api/v2/classes/BeaconSpawnPoint.php
+++ Website/api/v2/classes/BeaconSpawnPoint.php
@@ -10,8 +10,8 @@ class BeaconSpawnPoint extends BeaconBlueprint {
 	
 	protected static function SQLColumns() {
 		$columns = parent::SQLColumns();
-		$columns[] = '(SELECT array_to_json(array_agg(row_to_json(groups_template))) FROM (SELECT label, group_id, weight, array_to_json(ARRAY(SELECT creatures.path FROM spawn_point_creatures INNER JOIN creatures ON (spawn_point_creatures.creature_id = creatures.object_id) WHERE spawn_point_creatures.group_id = spawn_point_groups.group_id)) AS creatures FROM spawn_point_groups WHERE spawn_point_groups.spawn_point_id = spawn_points.object_id) AS groups_template) AS spawn_groups';
-		$columns[] = 'json_object(array(SELECT array[creatures.path, max_percentage::text] FROM spawn_point_limits INNER JOIN creatures ON (spawn_point_limits.creature_id = creatures.object_id) WHERE spawn_point_limits.spawn_point_id = spawn_points.object_id)) AS spawn_limits';
+		$columns[] = '(SELECT array_to_json(array_agg(row_to_json(sets_template))) FROM (SELECT spawn_set_id, label, weight, spawn_offset, min_distance_from_players_multiplier, min_distance_from_structures_multiplier, min_distance_from_tamed_dinos_multiplier, spread_radius, water_only_minimum_height, offset_before_multiplier, (SELECT array_to_json(array_agg(row_to_json(entries_template))) FROM (SELECT set_entry_id, creature_id, chance, override, min_level_multiplier, max_level_multiplier, min_level_offset, max_level_offset, spawn_offset, (SELECT array_to_json(array_agg(row_to_json(levels_template))) FROM (SELECT difficulty, min_level, max_level FROM spawn_point_set_entry_levels WHERE spawn_point_set_entry_levels.spawn_point_set_entry_id = spawn_point_set_entries.set_entry_id) AS levels_template) AS level_overrides FROM spawn_point_set_entries WHERE spawn_point_set_entries.spawn_point_set_id = spawn_point_sets.spawn_set_id) AS entries_template) AS entries, (SELECT array_to_json(array_agg(row_to_json(replacements_template))) FROM (SELECT set_replacement_id, target_creature_id, replacement_creature_id FROM spawn_point_set_replacements WHERE spawn_point_set_replacements.spawn_point_set_id = spawn_point_sets.spawn_set_id) AS replacements_template) AS replacements FROM spawn_point_sets WHERE spawn_point_sets.spawn_point_id = spawn_points.object_id) AS sets_template) AS spawn_sets';
+		$columns[] = 'json_object(array(SELECT array[creatures.object_id::text, max_percentage::text] FROM spawn_point_limits INNER JOIN creatures ON (spawn_point_limits.creature_id = creatures.object_id) WHERE spawn_point_limits.spawn_point_id = spawn_points.object_id)) AS spawn_limits';
 		return $columns;
 	}
 	
@@ -31,8 +31,18 @@ class BeaconSpawnPoint extends BeaconBlueprint {
 		if ($obj === null) {
 			return null;
 		}
-		$obj->groups = is_null($row->Field('spawn_groups')) ? null : json_decode($row->Field('spawn_groups'), true);
-		$obj->limits = is_null($row->Field('spawn_limits')) ? null : json_decode($row->Field('spawn_limits'), true);
+		$obj->groups = is_null($row->Field('spawn_sets')) ? null : json_decode($row->Field('spawn_sets'), true);
+		
+		if (is_null($row->Field('spawn_limits'))) {
+			$obj->limits = null;
+		} else {
+			$decoded = json_decode($row->Field('spawn_limits'), true);
+			$obj->limits = [];
+			foreach ($decoded as $creature_id => $weight) {
+				$obj->limits[$creature_id] = floatval($weight);
+			}
+		}
+		
 		return $obj;
 	}
 	
@@ -40,9 +50,9 @@ class BeaconSpawnPoint extends BeaconBlueprint {
 		$json = parent::jsonSerialize();
 		$json['resource_url'] = BeaconAPI::URL('/spawn_point/' . urlencode($this->ObjectID()));
 		if (is_null($this->groups) || count($this->groups) == 0) {
-			$json['spawns'] = null;
+			$json['sets'] = null;
 		} else {
-			$json['spawns'] = $this->groups;
+			$json['sets'] = $this->groups;
 		}
 		if (is_null($this->limits) || count($this->limits) == 0) {
 			$json['limits'] = null;
