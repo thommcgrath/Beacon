6f3724263f47a2e2d37678f99e2fb915b1f04093
Thom McGrath <thommcgrath@users.noreply.github.com>
2020-07-18 18:40:52 +0000

    Fixed issue with delta generator skipping content.

--- Website/api/v2/deltas.php
+++ Website/api/v2/deltas.php
@@ -2,29 +2,43 @@
 
 require(dirname(__FILE__) . '/loader.php');
 
-BeaconAPI::RequireParams('version', 'since');
+BeaconAPI::RequireParams('version');
 
 if (BeaconAPI::Method() !== 'GET') {
 	BeaconAPI::ReplyError('Method not allowed', null, 405);
 }
 
-$version = intval($_GET['version']);
-try {
-	$since = new DateTime($_GET['since']);
-} catch (Exception $err) {
-	BeaconAPI::ReplyError('Unable to parse timestamp', $err->getMessage(), 400);
-}
 $database = BeaconCommon::Database();
-$results = $database->Query('SELECT path, size FROM update_deltas WHERE version = $1 AND created > $2 ORDER BY created ASC;', $version, $since->format('Y-m-d H:i:sO'));
-$paths = [];
-$total = 0;
-while (!$results->EOF()) {
-	$paths[] = [
-		'url' => 'https://updates.beaconapp.cc' . $results->Field('path'),
-		'size' => $results->Field('size')
-	];
-	$total += $results->Field('size');
-	$results->MoveNext();
+$version = intval($_GET['version']);
+if (isset($_GET['since'])) {
+	try {
+		$since = new DateTime($_GET['since']);
+	} catch (Exception $err) {
+		BeaconAPI::ReplyError('Unable to parse timestamp', $err->getMessage(), 400);
+	}
+	$results = $database->Query('SELECT path, size FROM update_deltas WHERE version = $1 AND created > $2 ORDER BY created ASC;', $version, $since->format('Y-m-d H:i:sO'));
+	$paths = [];
+	$total = 0;
+	while (!$results->EOF()) {
+		$paths[] = [
+			'url' => 'https://updates.beaconapp.cc' . $results->Field('path'),
+			'size' => $results->Field('size')
+		];
+		$total += $results->Field('size');
+		$results->MoveNext();
+	}
+} else {
+	$results = $database->Query('SELECT EXTRACT(epoch FROM MAX(created)) AS newest FROM update_deltas WHERE version = $1;', $version);
+	$query = '';
+	$prefix = '';
+	if (BeaconCommon::InProduction() == false) {
+		$prefix = '/' . BeaconCommon::EnvironmentName();
+	}
+	if (is_null($results->Field('newest')) == false) {
+		$query = '?t=' . $results->Field('newest');
+	}
+	$paths = [['url' => 'https://updates.beaconapp.cc/v' . $version . $prefix . '/Complete.beacondata' . $query, 'size' => -1]];
+	$total = -1;
 }
 
 BeaconAPI::ReplySuccess(['files' => $paths, 'total_size' => $total]);
