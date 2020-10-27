87b9645592087904b2c1d767078d400022487b9f
Thom McGrath <thommcgrath@users.noreply.github.com>
2020-10-27 16:04:23 +0000

    Fixed listing of spawn set replacements in API v2.

--- Website/api/v2/classes/BeaconSpawnPoint.php
+++ Website/api/v2/classes/BeaconSpawnPoint.php
@@ -3,7 +3,7 @@
 class BeaconSpawnPoint extends BeaconAPI\SpawnPoint {
 	protected static function SQLColumns() {
 		$columns = parent::SQLColumns();
-		$columns[] = '(SELECT array_to_json(array_agg(row_to_json(sets_template))) FROM (SELECT spawn_point_set_id, label, weight, spawn_offset, min_distance_from_players_multiplier, min_distance_from_structures_multiplier, min_distance_from_tamed_dinos_multiplier, spread_radius, water_only_minimum_height, offset_before_multiplier, (SELECT array_to_json(array_agg(row_to_json(entries_template))) FROM (SELECT spawn_point_set_entry_id, creature_id, weight, override, min_level_multiplier, max_level_multiplier, min_level_offset, max_level_offset, spawn_offset, (SELECT array_to_json(array_agg(row_to_json(levels_template))) FROM (SELECT difficulty, min_level, max_level FROM spawn_point_set_entry_levels WHERE spawn_point_set_entry_levels.spawn_point_set_entry_id = spawn_point_set_entries.spawn_point_set_entry_id) AS levels_template) AS level_overrides FROM spawn_point_set_entries WHERE spawn_point_set_entries.spawn_point_set_id = spawn_point_sets.spawn_point_set_id) AS entries_template) AS entries, (SELECT array_to_json(array_agg(row_to_json(replacements_template))) FROM (SELECT spawn_point_set_replacement_id, target_creature_id, replacement_creature_id FROM spawn_point_set_replacements WHERE spawn_point_set_replacements.spawn_point_set_id = spawn_point_sets.spawn_point_set_id) AS replacements_template) AS replacements FROM spawn_point_sets WHERE spawn_point_sets.spawn_point_id = spawn_points.object_id) AS sets_template) AS spawn_sets';
+		$columns[] = '(SELECT array_to_json(array_agg(row_to_json(sets_template))) FROM (SELECT spawn_point_set_id, label, weight, spawn_offset, min_distance_from_players_multiplier, min_distance_from_structures_multiplier, min_distance_from_tamed_dinos_multiplier, spread_radius, water_only_minimum_height, offset_before_multiplier, (SELECT array_to_json(array_agg(row_to_json(entries_template))) FROM (SELECT spawn_point_set_entry_id, creature_id, weight, override, min_level_multiplier, max_level_multiplier, min_level_offset, max_level_offset, spawn_offset, (SELECT array_to_json(array_agg(row_to_json(levels_template))) FROM (SELECT difficulty, min_level, max_level FROM spawn_point_set_entry_levels WHERE spawn_point_set_entry_levels.spawn_point_set_entry_id = spawn_point_set_entries.spawn_point_set_entry_id) AS levels_template) AS level_overrides FROM spawn_point_set_entries WHERE spawn_point_set_entries.spawn_point_set_id = spawn_point_sets.spawn_point_set_id) AS entries_template) AS entries, (SELECT array_to_json(array_agg(row_to_json(replacements_template))) FROM (SELECT target_creature_id AS creature_id, json_object(array_agg(array[replacement_creature_id::text, weight::text])) AS choices FROM spawn_point_set_replacements WHERE spawn_point_set_replacements.spawn_point_set_id = spawn_point_sets.spawn_point_set_id GROUP BY target_creature_id) AS replacements_template) AS replacements FROM spawn_point_sets WHERE spawn_point_sets.spawn_point_id = spawn_points.object_id) AS sets_template) AS spawn_sets';
 		$columns[] = 'json_object(array(SELECT array[creatures.object_id::text, max_percentage::text] FROM spawn_point_limits INNER JOIN creatures ON (spawn_point_limits.creature_id = creatures.object_id) WHERE spawn_point_limits.spawn_point_id = spawn_points.object_id)) AS spawn_limits';
 		return $columns;
 	}
@@ -13,7 +13,26 @@ class BeaconSpawnPoint extends BeaconAPI\SpawnPoint {
 		if ($obj === null) {
 			return null;
 		}
-		$obj->groups = is_null($row->Field('spawn_sets')) ? null : json_decode($row->Field('spawn_sets'), true);
+		
+		if (is_null($row->Field('spawn_sets')) === false) {
+			$sets = json_decode($row->Field('spawn_sets'), true);
+			for ($i = 0; $i < count($sets); $i++) {
+				$replacements = $sets[$i]['replacements'];
+				if (is_null($replacements) === false) {
+					for ($j = 0; $j < count($replacements); $j++) {
+						$choices = $replacements[$j]['choices'];
+						foreach ($choices as $creature_id => $weight) {
+							$choices[$creature_id] = floatval($weight);
+						}
+						$replacements[$j]['choices'] = $choices;
+					}
+					$sets[$i]['replacements'] = $replacements;
+				}
+			}
+			$obj->groups = $sets;
+		} else {
+			$obj->groups = null;
+		}
 		
 		if (is_null($row->Field('spawn_limits'))) {
 			$obj->limits = null;
