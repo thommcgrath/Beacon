2c88c6bf37a881aaa3dd00f54f8ba6454d9c0b96
Thom McGrath <thommcgrath@users.noreply.github.com>
2021-02-23 19:55:33 +0000

    Updated welcome window with real content.

--- Website/www/welcome/index.php
+++ Website/www/welcome/index.php
@@ -19,12 +19,20 @@ function EndPage() {
 
 if ($original_version < 1) {
 	StartPage('Welcome to Beacon');
-	echo 'This is a work in progress. For now, <a href="/videos/introduction_to_loot_drops_with">watch this video</a> to learn about Beacon.';
+	echo '<div class="duo duo-left"><div class="duo-image"><a href="/videos/welcome_to_beacon" target="_blank"><img src="beacon15-video-thumb.png" width="298" height="167" alt="Welcome to Beacon Video"></a></div><div class="duo-text"><h1>Welcome to Beacon</h1><p>To help you get started with Beacon, you should watch this introduction video. You\'ll be shown how to navigate a Beacon project, create loot drops, update servers, and use config sets. It will 20 minutes well spent.</p></div></div>';
 	EndPage();
 } else {
 	if ($current_version >= 10500000 && $original_version < 10500000) {
-		StartPage('What\'s new in Beacon 1.5');
-		echo '<p>This is a work in progress. For now, <a href="/videos/whats_new_in_beacon_15">watch this video</a> for new features.</p><p>Since that video was produced, Beacon has added a Decay and Spoil editor, new breeding multipliers options, and password management in the Servers editor.</p>';
+		StartPage('New Look');
+		echo '<div class="duo duo-left"><div class="duo-image"><img src="beacon15-new-design.png" width="298" height="186" alt=""></div><div class="duo-text"><h1>Updated Design</h1><p>Beacon has grown a lot since the last time its layout was updated, and there is a lot more going on these days. The new design is faster to use, easier to discover, and more consistent.</p></div></div>';
+		EndPage();
+		
+		StartPage('Config Sets');
+		echo '<div class="duo duo-right"><div class="duo-image"><img src="beacon15-config-sets-menu.png" width="298" height="242" alt=""></div><div class="duo-text"><h1>Introducing Config Sets</h1><p>Config sets are one of the most powerful features ever introduced to Beacon. Now you can setup small batches of changes that can be quickly applied to servers, such as enabling PvP on weekends or running a breeding event. For admins of multiple servers, config sets allow servers to share common settings, while maintaining differences that make them unique.</p></div></div>';
+		EndPage();
+		
+		StartPage('Decay and Spoil Editor');
+		echo '<div class="duo duo-left"><div class="duo-image"><img src="beacon15-decay-spoil.png" width="298" height="195" alt=""></div><div class="duo-text"><h1>Decay and Spoil Editor</h1><p>The new Decay and Spoil editor allows easy control of structure decay and destruction, as well as spoil times, all with a nice chart to preview the effects. This new editor is free for all Beacon users.</div></div>';
 		EndPage();
 	}
 }
