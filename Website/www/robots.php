bded557d6b8afd775d2f7768d42f933cdf149007
Thom McGrath <thommcgrath@users.noreply.github.com>
2021-05-06 16:58:47 +0000

    Trying to shut up the damn MJ12bot

--- Website/www/robots.php
+++ Website/www/robots.php
@@ -4,6 +4,8 @@ require(dirname(__FILE__, 2) . '/framework/loader.php');
 
 header('Content-Type: text/plain');
 
+echo "User-agent: MJ12bot\nDisallow: /\n\n";
+
 if (BeaconCommon::InProduction()) {
 	echo "User-agent: *\nAllow: /";
 } else {
