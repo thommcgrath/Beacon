48b2c160c798a6fd8d8720151d870692995d5e7b
Thom McGrath <thommcgrath@users.noreply.github.com>
2021-05-07 20:16:36 +0000

    Blocking assets with robots.txt probably wasn't the best idea.

--- Website/www/robots.php
+++ Website/www/robots.php
@@ -4,7 +4,7 @@ require(dirname(__FILE__, 2) . '/framework/loader.php');
 
 header('Content-Type: text/plain');
 
-echo "User-agent: *\nDisallow: /download/\nDisallow: /assets/\n";
+echo "User-agent: *\nDisallow: /download/\n";
 
 if (BeaconCommon::InProduction()) {
 	echo 'Allow: /';
