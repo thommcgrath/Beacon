d9f44edb13d4ec15e4fcddd2222d6b190a2b4ef0
Thom McGrath <thommcgrath@users.noreply.github.com>
2022-05-06 00:26:15 +0000

    More resilient legacy blueprint recovery script.

--- Website/www/account/repair_blueprints.php
+++ Website/www/account/repair_blueprints.php
@@ -13,9 +13,32 @@ $user = BeaconUser::GetByUserID($session->UserID());
 $user_id = $user->UserID();
 $legacy_path = '/' . $user_id . '/Blueprints.json';
 
-$legacy_contents = BeaconCloudStorage::GetFile($legacy_path);
-if (empty($legacy_contents)) {
+$versions = BeaconCloudStorage::VersionsForFile($legacy_path);
+if (count($versions) === 0) {
 	echo 'There are no legacy blueprints to migrate';
+	exit;
+}
+
+$latest_version_id = null;
+$latest_version_date = null;
+foreach ($versions as $versioninfo) {
+	$date = strtotime(substr($versioninfo['date'], 0, 19));
+	if ($date === false) {
+		continue;
+	}
+	if (is_null($latest_version_date) || $date > $latest_version_date) {
+		$latest_version_date = $date;
+		$latest_version_id = $versioninfo['version_id'];
+	}
+}
+if (is_null($latest_version_id)) {
+	echo 'Could not determine latest blueprint version to recover.';
+	exit;
+}
+
+$legacy_contents = BeaconCloudStorage::GetFile($legacy_path, false, $latest_version_id);
+if (empty($legacy_contents)) {
+	echo 'Failed to recover legacy blueprints file.';
 	exit;
 }
 
