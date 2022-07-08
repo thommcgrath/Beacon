190ea72d735cdbe880a454b343c0bd042a1e751f
Thom McGrath <thommcgrath@users.noreply.github.com>
2022-07-08 18:19:20 +0000

    Added uwp_change info to ini options.

--- Website/api/v3/ark/classes/ConfigLine.php
+++ Website/api/v3/ark/classes/ConfigLine.php
@@ -3,9 +3,27 @@
 namespace Ark;
 
 class ConfigLine extends \BeaconAPI\Ark\ConfigLine {
+	protected ?array $uwp_changes;
+	
+	protected static function SQLColumns() {
+		$columns = parent::SQLColumns();
+		$columns[] = 'uwp_changes';
+		return $columns;
+	}
+	
+	protected static function FromRow(\BeaconRecordSet $row) {
+		$obj = parent::FromRow($row);
+		if ($obj === null) {
+			return null;
+		}
+		$obj->uwp_changes = is_null($row->Field('uwp_changes')) ? null : json_decode($row->Field('uwp_changes'), true);
+		return $obj;
+	}
+	
 	public function jsonSerialize() {
 		$json = parent::jsonSerialize();
 		$json['resource_url'] = \BeaconAPI::URL('ark/ini_option/' . urlencode($this->ObjectID()));
+		$json['uwp_changes'] = $this->uwp_changes;
 		return $json;
 	}
 }
