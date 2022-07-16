b9784fb4f88365ed4d634a12baa74bc7169b11d3
Thom McGrath <thommcgrath@users.noreply.github.com>
2022-07-16 06:01:45 +0000

    Finishing git pull script.

--- Website/scripts/pull_git.php
+++ Website/scripts/pull_git.php
@@ -7,20 +7,37 @@ while (ob_get_level() > 0) {
 	ob_end_clean();
 }
 
-$database = BeaconCommon::Database();
-$results = $database->Query('SELECT hash FROM public.endpoint_git_hashes WHERE hostname = $1 AND environment = $2;', gethostname(), BeaconCommon::EnvironmentName());
-if ($results->RecordCount() === 0) {
-	echo "No records for this host.\n";
+$root = dirname(__FILE__, 3);
+$return_code = 0;
+ob_start();
+system('cd ' . escapeshellarg($root) . ' && git rev-parse --abbrev-ref HEAD', $return_code);
+$current_branch = trim(ob_get_contents());
+ob_end_clean();
+if ($return_code !== 0) {
+	echo "Failed to get current branch:\n";
+	echo "$current_branch\n";
 	exit;
 }
 
-$root = dirname(__FILE__, 3);
+$database = BeaconCommon::Database();
+$results = $database->Query('SELECT hash FROM public.endpoint_git_hashes WHERE branch = $1;', $current_branch);
+if ($results->RecordCount() === 0) {
+	echo "No records for branch $current_branch.\n";
+	exit;
+}
+
+
 $desired_hash = $results->Field('hash');
 $return_code = 0;
 ob_start();
 system('cd ' . escapeshellarg($root) . ' && git rev-parse HEAD', $return_code);
 $current_hash = trim(ob_get_contents());
 ob_end_clean();
+if ($return_code !== 0) {
+	echo "Failed to get current hash:\n";
+	echo "$current_hash\n";
+	exit;
+}
 
 if ($current_hash === $desired_hash) {
 	echo "Already up to date.\n";
