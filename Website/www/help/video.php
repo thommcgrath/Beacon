aa3c59a2b51009a2174e0c940c34df7ff062cb51
Thom McGrath <thommcgrath@users.noreply.github.com>
2022-09-19 05:09:52 +0000

    Added guidance when video playback fails.

--- Website/www/help/video.php
+++ Website/www/help/video.php
@@ -21,6 +21,57 @@ if ($results->RecordCount() == 0) {
 echo '<h1>' . htmlentities($results->Field('video_title')) . '</h1>';
 BeaconTemplate::SetTitle($results->Field('video_title'));
 
+$video_link = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+BeaconTemplate::StartScript(); ?>
+<script>
+const beacon = {
+	h264Tested: false,
+	h264Supported: undefined,
+	testH264: function() {
+		if (this.h264Tested) {
+			return;
+		}
+		
+		const testPlayer = document.createElement( "video" );
+		if (testPlayer.canPlayType) {
+			this.h264Supported = Boolean(testPlayer.canPlayType('video/mp4; codecs="avc1.42C01E"'));
+		}
+		this.h264Tested = true;
+	},
+	pageLoaded: function(buildNumber) {
+		this.testH264();
+		
+		if (!this.h264Supported) {
+			executeInXojo('openInBrowser', <?php echo json_encode($video_link); ?>);
+		}
+	}
+};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+	beacon.testH264();
+	
+	if (!beacon.h264Supported) {
+		const warning = document.getElementById('playback-warning');
+		warning.classList.remove('hidden');
+	}
+	
+	const copyLinkButton = document.getElementById('copy-video-link');
+	if (copyLinkButton) {
+		copyLinkButton.addEventListener('click', (event) => {
+			event.preventDefault();
+			navigator.clipboard.writeText(<?php echo json_encode($video_link); ?>);
+			event.target.innerText = 'Link Copied';
+			return false;
+		});
+	}
+});
+</script>
+<?php
+BeaconTemplate::FinishScript();
+	
+echo '<div class="notice-block notice-warning text-center hidden" id="playback-warning">If you experience playback problems, visit ' . $video_link . ' with your browser. <a href="#" id="copy-video-link">Copy Link</a></div>';
+
 switch ($results->Field('host')) {
 case 'YouTube':
 	echo '<div class="embedded_youtube_video"><iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/' . $results->Field('host_video_id') . '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>';
