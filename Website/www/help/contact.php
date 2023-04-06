690911daa32f188ba1c94e28a4e1ae6210b14c45
Thom McGrath <thommcgrath@users.noreply.github.com>
2023-04-06 19:43:42 +0000

    Fixed contact page scripts

--- Website/www/help/contact.php
+++ Website/www/help/contact.php
@@ -2,7 +2,7 @@
 require(dirname(__FILE__, 3) . '/framework/loader.php');
 BeaconTemplate::SetTitle('Submit Ticket');
 BeaconTemplate::SetPageDescription('Submit a new support request');
-BeaconTemplate::AddScript('/assets/scripts/contact.js');
+BeaconTemplate::AddScript(BeaconCommon::AssetURI('contact.js'));
 
 $current_timestamp = time();
 $psk = BeaconCommon::GetGlobal('Support Ticket Key');
