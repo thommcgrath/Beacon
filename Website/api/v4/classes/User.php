d241c4e8887ef7d7b877b85cee68e2c257dba7ce
Thom McGrath <thommcgrath@users.noreply.github.com>
2023-08-01 17:01:07 +0000

    Updated password validation for v4 too

--- Website/api/v4/classes/User.php
+++ Website/api/v4/classes/User.php
@@ -457,21 +457,7 @@ class User extends DatabaseObject implements JsonSerializable {
 	}
 	
 	public static function ValidatePassword(string $password): bool {
-		$passlen = strlen($password);
-		
-		if ($passlen < 8 || $passlen > 256) {
-			return false;
-		}
-		
-		$chars = count_chars($password);
-		foreach ($chars as $char => $count) {
-			$percent = $count / $passlen;
-			if ($percent > 0.3) {
-				return false;
-			}
-		}
-		
-		return true;
+		return mb_strlen($password) >= 8;
 	}
 	
 	public static function IsExtendedUsername(string $username): bool {
