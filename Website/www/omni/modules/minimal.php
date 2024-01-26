de05af0a2b316ed1d8fc7dd089b90eaa1847907a
Thom McGrath <thommcgrath@users.noreply.github.com>
2024-01-26 21:44:25 +0000

    Updated Omni page

---
+++ Website/www/omni/modules/minimal.php
@@ -0,0 +1,7 @@
+<h1>Beacon supports <span class="game-name"><?php echo htmlentities($gameName); ?></span> with a '<span class="license-name">Beacon Omni for Minimal Games</span>' license.</h1>
+<p>The free version of Beacon does not support this game.</p>
+<p>A '<span class="license-name">Beacon Omni for Minimal Games</span>' license supports the following games:</p>
+<ul>
+	<li>Palworld</li>
+	<li>More games coming in the future</li>
+</ul>
