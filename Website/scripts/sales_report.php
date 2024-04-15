9305cd28b8e5bf3ddb3702bfb7a05341ba724aa6
Thom McGrath <thommcgrath@users.noreply.github.com>
2024-04-15 21:47:32 +0000

    Updated daily sales report

--- Website/scripts/sales_report.php
+++ Website/scripts/sales_report.php
@@ -8,7 +8,7 @@ while (ob_get_level() > 0) {
 }
 
 define('SQL_DATE_FORMAT', 'Y-m-d H:i:sO');
-$query = "SELECT SUM(total_paid_usd - tax_usd) AS total_usd, COUNT(purchase_id) AS num_sales FROM purchases WHERE purchase_date >= $1 AND purchase_date < $2 AND refunded = FALSE AND total_paid > 0;";
+$query = "SELECT COALESCE(SUM(purchases.total_paid_usd - purchases.tax_usd), 0) AS total_usd, COUNT(purchases.purchase_id) AS num_sales FROM public.purchases WHERE purchases.purchase_date >= $1 AND purchases.purchase_date < $2 AND purchases.refunded = FALSE AND purchases.total_paid > 0;";
 $database = BeaconCommon::Database();
 $timezone = new DateTimeZone('America/New_York');
 $today = new DateTime('now', $timezone);
@@ -34,11 +34,11 @@ $rollingEnd = clone $today;
 $rollingRows = $database->Query("SELECT COALESCE(SUM(purchases.total_paid_usd - purchases.tax_usd), 0) AS total_usd FROM public.purchases WHERE purchases.purchase_date >= $1 AND purchases.purchase_date < $2 AND purchases.refunded = FALSE AND purchases.total_paid > 0;", $rollingStart->format(SQL_DATE_FORMAT), $rollingEnd->format(SQL_DATE_FORMAT));
 $rollingTotal = $rollingRows->Field('total_usd');
 $rollingAverage = $rollingTotal / $rollingDays;
-$projection = $rollingAverage * $daysInMonth;
 
 $gameRows = $database->Query("SELECT game_id FROM public.games WHERE public = TRUE ORDER BY game_id;");
 
 $fields = [];
+$figures = [];
 $dailyTotal = RunReport("Day's", null, $yesterday, $today, $fields);
 $monthTotal = RunReport('MTD', null, $firstOfMonth, $lastOfMonth, $fields);
 while (!$gameRows->EOF()) {
@@ -47,6 +47,9 @@ while (!$gameRows->EOF()) {
 	RunReport("MTD {$gameId}", $gameId, $firstOfMonth, $lastOfMonth, $fields, $monthTotal);
 	$gameRows->MoveNext();
 }
+
+$remainingDays = ($daysInMonth - $today->format('d')) + 1; // Removes today, so +1 puts it back.
+$projection = $figures['MTD'] + ($rollingAverage * $remainingDays);
 $fields[] = [
 	'title' => "Daily Average",
 	'value' => '$' . number_format($rollingAverage, 2),
@@ -56,6 +59,22 @@ $fields[] = [
 	'title' => "{$monthName} Projection",
 	'value' => '$' . number_format($projection, 2),
 	'short' => true
+];
+
+$firstOfYear = new DateTime('first day of january this year', $timezone);
+$firstOfNextYear = new DateTime('first day of january next year', $timezone);
+$yearRows = $database->Query($query, $firstOfYear->format(SQL_DATE_FORMAT), $firstOfNextYear->format(SQL_DATE_FORMAT));
+$yearToDate = $yearRows->Field('total_usd');
+$estimatedTaxes = $yearToDate * 0.26;
+$fields[] = [
+	'title' => "Year To Date",
+	'value' => '$' . number_format($yearToDate, 2),
+	'short' => true,
+];
+$fields[] = [
+	'title' => "Estimated Taxes",
+	'value' => '$' . number_format($estimatedTaxes, 2),
+	'short' => true,
 ];
 
 $arr = [
@@ -70,19 +89,23 @@ $arr = [
 if (BeaconCommon::InProduction()) {
 	BeaconCommon::PostSlackRaw(json_encode($arr));
 } else {
+	echo json_encode($figures, JSON_PRETTY_PRINT);
 	echo json_encode($arr, JSON_PRETTY_PRINT);
 }
 
 function RunReport(string $label, ?string $gameId, DateTime $periodStart, DateTime $periodEnd, array &$fields, ?float $periodTotal = null): float {
+	global $query, $figures;
+
 	$database = BeaconCommon::Database();
 	if (is_null($gameId)) {
-		$rows = $database->Query("SELECT COALESCE(SUM(purchases.total_paid_usd - purchases.tax_usd), 0) AS total_usd, COUNT(purchases.purchase_id) AS num_sales FROM public.purchases WHERE purchases.purchase_date >= $1 AND purchases.purchase_date < $2 AND purchases.refunded = FALSE AND purchases.total_paid > 0;", $periodStart->format(SQL_DATE_FORMAT), $periodEnd->format(SQL_DATE_FORMAT));
+		$rows = $database->Query($query, $periodStart->format(SQL_DATE_FORMAT), $periodEnd->format(SQL_DATE_FORMAT));
 	} else {
 		$rows = $database->Query("SELECT COALESCE(SUM(purchase_items.line_total_usd - purchase_items.tax_usd), 0) AS total_usd, COUNT(purchase_items.line_id) AS num_sales FROM public.purchase_items INNER JOIN public.purchases ON (purchase_items.purchase_id = purchases.purchase_id) WHERE purchases.purchase_date >= $1 AND purchases.purchase_date < $2 AND purchases.refunded = FALSE AND purchases.total_paid > 0 AND purchase_items.product_id IN (SELECT products.product_id FROM public.products WHERE game_id = $3);", $periodStart->format(SQL_DATE_FORMAT), $periodEnd->format(SQL_DATE_FORMAT), $gameId);
 	}
 
 	$total = $rows->Field('total_usd');
 	$numSales = number_format($rows->Field('num_sales'));
+	$figures[$label] = $total;
 	$salesFormatted = '$' . number_format($total, 2);
 	if (is_null($gameId) === false) {
 		if ($periodTotal > 0) {
