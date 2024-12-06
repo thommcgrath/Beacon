e6d3000818826cf5519741489a2369d51d9271f2
Thom McGrath <thommcgrath@users.noreply.github.com>
2024-12-06 20:48:25 +0000

    Cloud storage will try to fetch from backup buckets if primary is down

--- Website/scripts/run_usercloud_queue.php
+++ Website/scripts/run_usercloud_queue.php
@@ -3,6 +3,14 @@
 
 require(dirname(__FILE__, 2) . '/framework/loader.php');
 
+$username = exec('whoami');
+if ($username !== 'nginx') {
+	echo "This command should be run as nginx.\n";
+	echo "Currently running as {$username}\n";
+	echo 'sudo su -s /bin/bash -c "' . __FILE__ . '" nginx;' . "\n";
+	exit;
+}
+
 BeaconCloudStorage::RunQueue();
 
 ?>
